51262b266252cf1d0fecdc9278afdf83678d18be
Scott Zackrison <cszackrison@gmail.com>
2026-08-07 11:36:58 -0600

    Add color-coded status segments

--- config.h
+++ config.h
@@ -10,25 +10,28 @@ static const char unknown_str[] = "n/a";
 /* maximum output string length */
 #define MAXLEN 2048
 
+/* padding inside status segments */
+#define PAD " "
+
 /* output formats */
 static const struct arg args[] = {
 	/* function, format, argument */
-	{ herdr_status, "| AGENTS %s ", NULL, 1 },
-	{ claude_subscription_usage, "| ANT %s ", NULL, 1 },
-	{ openai_subscription_usage, "| OAI %s ", NULL, 1 },
+	{ herdr_status, "\x05" PAD "AGENTS %s ", NULL, 1 },
+	{ claude_subscription_usage, "\x06" PAD "ANT %s ", NULL, 1 },
+	{ openai_subscription_usage, "\x07" PAD "OAI %s ", NULL, 1 },
 	/*{ disk_free, " %.5s GiB | ", "/" },
 	{ ipv4, "E: %s | ", "enp9s0" },*/
 	/*{ wifi_perc, "W: (%3s%% on ", "wlp8s0" },
 	{ wifi_essid, "%s) ", "wlp8s0" },*/
 	/*{ ipv4, "%s | ", "wlp8s0" },*/
-	{ vol_status, "| \x01%s  ", NULL, 0 },
-	{ mic_status, "\x04%s  ", NULL, 1 },
-	{ razer_dpi, "\x02󰍽 %s DPI  ", "0003:1532:0083.", 1 },
-	{ razer_battery, "󰁹 %s%%  ", "0003:1532:0083.", 1 },
+	{ razer_dpi, "\x02" PAD "󰍽 %s DPI", "0003:1532:0083.", 1 },
+	{ razer_battery, "\x08" PAD "󰁹 %s%%", "0003:1532:0083.", 1 },
 	/*{ battery_state, "%s ", "BAT1" },*/
 	/*{ battery_perc, "%3s%% | ", "BAT1" },*/
-	{ temp_fallback, "\x03󰔏 %s° | ", "/sys/class/thermal/thermal_zone2/temp,hwmon=k10temp,hwmon=coretemp,/sys/class/thermal/thermal_zone0/temp", 0 },
-	{ datetime, "%s", "%D %a %I:%M", 0 },
+	{ vol_status, "\x01" PAD "%s", NULL, 0 },
+	{ mic_status, "\x04" PAD "%s", NULL, 1 },
+	{ temp_fallback, "\x03" PAD "󰔏 %s°", "/sys/class/thermal/thermal_zone2/temp,hwmon=k10temp,hwmon=coretemp,/sys/class/thermal/thermal_zone0/temp", 0 },
+	{ datetime, "\x09" PAD " %s ", "%D %a %I:%M", 0 },
 };
 
 /*
