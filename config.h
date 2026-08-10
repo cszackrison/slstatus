8c3dd937aa6c79ec31b143a2733708165bfd4cae
Scott Zackrison <cszackrison@gmail.com>
2026-08-10 15:50:22 -0600

    Use Nerd Font icons for AI usage labels

--- config.h
+++ config.h
@@ -17,8 +17,8 @@ static const char unknown_str[] = "n/a";
 static const struct arg args[] = {
 	/* function, format, argument */
 	{ herdr_status, "\x05" PAD "AGENTS %s ", NULL, 1 },
-	{ claude_subscription_usage, "\x06" PAD "ANT %s ", NULL, 1 },
-	{ openai_subscription_usage, "\x07" PAD "OAI %s ", NULL, 1 },
+	{ claude_subscription_usage, "\x06" PAD " %s ", NULL, 1 },
+	{ openai_subscription_usage, "\x07" PAD " %s ", NULL, 1 },
 	/*{ disk_free, " %.5s GiB | ", "/" },
 	{ ipv4, "E: %s | ", "enp9s0" },*/
 	/*{ wifi_perc, "W: (%3s%% on ", "wlp8s0" },
